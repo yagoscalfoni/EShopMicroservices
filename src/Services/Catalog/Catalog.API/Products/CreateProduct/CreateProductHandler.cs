821b110a07b0c25f11bdb272f04b5d9c113b94b1
Yago Escalfoni <yago@otimagestao.com>
2024-04-24 23:05:14 -0300

    catalog-crud

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,19 +1,14 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
-
-namespace Catalog.API.Products.CreateProduct
+﻿namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler
+    internal class CreateProductCommandHandler(IDocumentSession session)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
             // Create Product Entity from command object
-            // Save to database
             // Return CreateProductResult result
 
             var product = new Product
@@ -25,7 +20,12 @@ namespace Catalog.API.Products.CreateProduct
                 Price = command.Price
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            // Save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            // Return result
+            return new CreateProductResult(product.Id);
 
 
         }
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(Id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
+    }
+}
