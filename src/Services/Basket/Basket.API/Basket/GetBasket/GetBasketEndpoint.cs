8a7c8959addd52e9fcbe7f6245f4e8339dd206f9
Yago Escalfoni <yago@otimagestao.com>
2024-05-02 23:23:31 -0300

    feature: crud operations and creation of basket microservice

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketResponse(ShoppingCart Cart);
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new GetBasketQuery(userName));
+
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+                .WithName("GetProductById")
+                .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product By Id")
+                .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using BuildingBlocks.CQRS;
+global using Basket.API.Models;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
