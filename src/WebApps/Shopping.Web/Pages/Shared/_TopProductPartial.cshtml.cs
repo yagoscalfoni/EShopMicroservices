7b6b3e96f3dbd00024f699292da4828e9a305e4e
Yago Escalfoni <yago@otimagestao.com>
2024-05-24 01:11:44 -0300

    feature: yarp gateway, web application

---
+++ src/WebApps/Shopping.Web/Pages/Shared/_TopProductPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopping.Web.Pages.Shared
+{
+    public class _TopProductPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
