7cfdd5e1f60d937461b5428b6c5ef7365acb166c
Yago Scalfoni <yago.escalfoni2020.dev@outlook.com>
2024-11-04 23:31:04 -0300

    monitoring api service

---
+++ src/BuildingBlocks/BuildingBlocks/Monitoring/MonitoringSetup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using Serilog;
+
+namespace BuildingBlocks.Monitoring
+{
+    public static class MonitoringSetup
+    {
+        public static void AddMonitoring(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configuração do Serilog para logs estruturados no console
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.Console()  // Envia logs para o console
+                .CreateLogger();
+
+            // Adiciona Serilog como o logger da aplicação
+            services.AddLogging(loggingBuilder =>
+                loggingBuilder.AddSerilog(dispose: true));
+        }
+    }
+}
--- src/Services/User/User.API/Program.cs
+++ src/Services/User/User.API/Program.cs
@@ -2,13 +2,18 @@ using User.API;
 using User.Application;
 using User.Infrastructure;
 using User.Infrastructure.Data.Extensions;
+using BuildingBlocks.Monitoring;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
-    .AddApiServices(builder.Configuration);
+    .AddApiServices(builder.Configuration)
+    .AddMonitoring(builder.Configuration);
 
 // Add services to the container.
 
@@ -17,7 +22,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Adiciona o OpenTelemetry para tracing e métricas
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("User.API"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri("http://jaeger:4317"); // Porta OTLP do Jaeger
+            });
+    })
+    .WithMetrics(metricProviderBuilder =>
+    {
+        metricProviderBuilder
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddPrometheusExporter(); // Expor métricas para Prometheus
+    });
+
+// Expor o endpoint de scraping do Prometheus para métricas
 var app = builder.Build();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
