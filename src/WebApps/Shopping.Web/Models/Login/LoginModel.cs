12e0c13273ce2c7b6f209187831f603aeed97621
Yago Scalfoni <74843126+yagoscalfoni@users.noreply.github.com>
2025-12-30 21:22:10 -0300

    Add validation to login form

--- src/WebApps/Shopping.Web/Models/Login/LoginModel.cs
+++ src/WebApps/Shopping.Web/Models/Login/LoginModel.cs
@@ -1,9 +1,16 @@
-﻿namespace Shopping.Web.Models.Login
+using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Web.Models.Login
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "O email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "Informe um email válido.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
         public string Password { get; set; }
+
         public bool RememberMe { get; set; }
     }
 }
