b6fb7a20e0528147401379a767d4c6a62cae74f9
Yago Escalfoni <yago.escalfoni.dev2025@outlook.com>
2026-01-03 16:30:49 -0300

    Ajusta import do environment e adiciona Dockerfile Angular
    - Corrige o path de importação do environment para absoluto em api.config.ts, melhorando a compatibilidade de build.
    - Adiciona Dockerfile multi-stage para build e deploy da aplicação Angular com Node.js e Nginx.

--- src/WebApps/Shopping.Angular/src/app/core/config/api.config.ts
+++ src/WebApps/Shopping.Angular/src/app/core/config/api.config.ts
@@ -1,4 +1,4 @@
-import { environment } from '../../environments/environment';
+import { environment } from "src/environments/environment";
 
 export const API_BASE_URL = environment.production
   ? environment.gatewayUrls.production
